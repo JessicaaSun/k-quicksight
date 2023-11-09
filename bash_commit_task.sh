0706dc70d024194ba6ed55845b60d58c9ba03460
seangleng <seangleng507@gmail.com>
2023-11-09 11:52:51 +0700

    i have mofidy about us page at the button link svg and created sign up with successfully, and modify file with added [email] folder into veify folder

--- bash_commit_task.sh
+++ bash_commit_task.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 git add .
-# shellcheck disable=SC2034
-# shellcheck disable=SC2162
 read -p "Enter your commit status: " commit
-# shellcheck disable=SC2016
-git commit -m '$commit'
+git commit -m "$commit"
 
-# shellcheck disable=SC2162
-# shellcheck disable=SC2034
 read -p "Enter your repository name: " name
-# shellcheck disable=SC2016
-git push origin '$name'
+git push origin "$name"

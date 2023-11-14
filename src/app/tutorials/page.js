d2de3f134834dac655bb6a54299f420af32234b1
sirblue <sophearum14@gmail.com>
2023-11-14 08:27:13 +0700

    request toturial have done

--- src/app/tutorials/page.js
+++ src/app/tutorials/page.js
@@ -5,6 +5,10 @@ import {ErrorMessage, Field, Form, Formik} from "formik";
 import {Button} from "@nextui-org/react";
 import Image from "next/image";
 import recommend from '@assets/images/recommend.png'
+import {useCreateRequestTutorialMutation} from "@/store/features/request_tutorial/requestTutorialApiSlice";
+import {useGetUserQuery} from "@/store/features/user/userApiSlice";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Tutorial = () => {
 
@@ -39,6 +43,28 @@ const Tutorial = () => {
         },
     ]
 
+    // handle user input
+    const [sendRequestTutorial] = useCreateRequestTutorialMutation()
+    const {data: user} = useGetUserQuery();
+
+    const handleSubmit = async (values) => {
+        values.request_by=user?.data?.id
+
+        const submit = await sendRequestTutorial({data: values});
+        if (submit?.data){
+            toast.success("Request Tutorial successfully.")
+        }
+    }
+    // toast message
+    // const notify = (values) => {
+    //     if (values && values.subject && values.message){
+    //         return true
+    //     }else {
+    //         toast.error("Please input the field!");
+    //         return false
+    //     }
+    // }
+
     return (
         <section className={'py-44 px-[10%]'}>
             <h1 className={'text-bold text-primary-color pt-10 pb-10'}>Tutorials</h1>
@@ -72,23 +98,26 @@ const Tutorial = () => {
                 <Formik
                     initialValues={
                         {
-                            subject: '',
-                            message: ''
+                            request_by: user?.data?.id,
+                            message: '',
+                            subject: ''
                         }
                     }
                     validate={(values) => {
                         const errors = {}
                         if (!values.subject) {
-                            errors.subject = 'We need you subject!'
+                            errors.subject = 'Please input the subject!'
                         }
                         if (!values.message){
                             errors.message = 'Message required'
                         }
                         return errors;
                     }}
-                    onSubmit={(values, {setSubmitting}) => {
-                        console.log(`hello ${values.subject} you have send ${values.message}`);
+                    onSubmit={(values, {setSubmitting,resetForm }) => {
+                        handleSubmit(values);
                         setSubmitting(false);
+                        resetForm();
+
                     }}>
                     {({ isSubmitting }) => (
                         <Form className={'py-10 flex flex-col gap-10 relative'}>
@@ -103,18 +132,18 @@ const Tutorial = () => {
                             </div>
                             <div>
                                 <p className={'text-lg text-text-color font-semibold'}>Message</p>
-                                <Field as={'textarea'} className={'w-full h-[200px] overflow-y-scroll resize-y px-5 py-5 my-3 bg-gray-50 rounded-xl border-2 border-gray-400'} type={'text'} name={'message'} placeholder={'Example: I recommend you explain me step by step about this subject and give me the sample with the real data..  '} />
+                                <Field as={'textarea'} className={'w-full h-[200p x] overflow-y-scroll resize-y px-5 py-5 my-3 bg-gray-50 rounded-xl border-2 border-gray-400'} type={'text'} name={'message'} placeholder={'Example: I recommend you explain me step by step about this subject and give me the sample with the real data..  '} />
                                 <ErrorMessage
                                     className={'text-red-500'}
                                     name={"message"}
                                     component={"div"}
                                 />
                             </div>
+                            <ToastContainer/>
                             <Button type={"submit"} disabled={isSubmitting} className={'font-bold text-white bg-primary-color absolute right-0 -bottom-5 rounded-xl w-fit py-2 px-12'}>Send</Button>
                         </Form>
                     )}
                 </Formik>
-
             </div>
         </section>
     );

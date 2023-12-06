c1f77598b667d20587e62a741bb3ea923b346618
sirblue <sophearum14@gmail.com>
2023-12-06 13:57:18 +0700

    added sample

---
+++ src/app/sample/components/JupiterDoc.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function JupiterDoc() {
+    return (
+        <div className="py-6">
+            <iframe
+                    width="100%"
+                    height="100%"
+                    src="https://photostad-api.istad.co/api/v1/jupyter/notebook/82c3bbb31229421895f03fb75feac4ab.html/"
+                    title="YouTube video player"
+            >
+            </iframe>
+        </div>
+    );
+}

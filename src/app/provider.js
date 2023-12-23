b150dd5834c534c781772a011022109b487e33e9
seangleng <seangleng507@gmail.com>
2023-12-23 21:20:26 +0700

    update dark mode

--- src/app/provider.js
+++ src/app/provider.js
@@ -4,11 +4,15 @@ import { SessionProvider } from "next-auth/react";
 
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
+import {ThemeProvider as NextThemesProvider} from "next-themes";
 
 export default function Provider({ children }) {
   return (
     <NextUIProvider>
-      <SessionProvider>{children}</SessionProvider>
+        <NextThemesProvider attribute="class" defaultTheme="light">
+            <SessionProvider>{children}</SessionProvider>
+        </NextThemesProvider>
+
     </NextUIProvider>
   );
 }

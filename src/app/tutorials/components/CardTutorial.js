a1febff6d5438ec764875fb9c81869bc9dcfe8ff
PhonSobon <phon.sobon02@gmail.com>
2023-12-28 15:10:50 +0700

    reponsive card tutorial

--- src/app/tutorials/components/CardTutorial.js
+++ src/app/tutorials/components/CardTutorial.js
@@ -19,30 +19,34 @@ const CardTutorial = () => {
         router.push(`/tutorials/${id}`)
     }
     return (
-        <div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+        <div className={"md:grid md:gap-5 lg:gap-8 grid-cols-2 "}>
             {
                 tutorials?.results.map((e, index) => (
                     <div className="cursor-pointer md:pt-0 pt-5" key={index} onClick={()=>router.push(`/tutorials/${e.uuid}`)}>
-                        <div class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
-                            <Image
-                                width={100}
-                                height={100}
-                                className={"object-cover h-full w-full"}
-                                unoptimized={true}
-                                src={generateBashURL(e.thumbnail)}
-                                alt="card-image"/>
-                        </div>
-                        <div class="p-6">
-                            <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                                {e.title}
-                            </h5>
-                            <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                                {e.description}
-                            </p>
+                        <div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-[80%]">
+                            <div class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+                                <Image
+                                    width={100}
+                                    height={100}
+                                    className={"object-cover h-full w-full"}
+                                    unoptimized={true}
+                                    src={generateBashURL(e.thumbnail)}
+                                    alt="card-image"/>
+                            </div>
+                            <div class="p-6">
+                                <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                                    {e.title}
+                                </h5>
+                                <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+                                    {e.description}
+                                </p>
+                            </div>
+
                         </div>
                     </div>
                 ))
             }
+
         </div>
 
     );
